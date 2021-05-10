234501c84fd2b4b329618e3563f59923f11cf1ad
Lisi Linhart <ewl@storyblok.com>
2021-05-10 14:37:23 +0200

    remove old files

--- src/components/component_not_found.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const ComponentNotFound = (props) => (
-  <div>
-    Component {props.blok.component} is not defined. Add it to components.js
-  </div>
-)
-
-export default ComponentNotFound
