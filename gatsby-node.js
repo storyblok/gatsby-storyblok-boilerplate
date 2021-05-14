8598cae3b13f43ff314c8e25e997a7c3204f53ac
Francis Champagne <accounts@fcis.io>
2021-05-14 18:22:29 -0400

    refactor: general improvements

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,55 +4,58 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
- const path = require('path')
+const path = require("path")
 
- exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions
+const getByComponent = (query, type) =>
+  query.filter(({ node }) => node.field_component === type)
 
-    return new Promise((resolve, reject) => {
-        // sets the template for the pages
-        const storyblokEntry = path.resolve('src/templates/page.js')
-    
-        // gets all storyblok stories with the content type 'page'
-        resolve(
-          graphql(
-            `{
-              stories: allStoryblokEntry(filter: {field_component: {eq: "page"}}) {
-                edges {
-                  node {
-                    id
-                    name
-                    slug
-                    field_component
-                    full_slug
-                    content
-                  }
-                }
-              }
-            }`
-          ).then(result => {
-            if (result.errors) {
-              console.log(result.errors)
-              reject(result.errors)
-            }
-    
-            const entries = result.data.stories.edges
-            
-            // creates a page for each entry with the storyblok slug
-            entries.forEach((entry) => {
-                // skip home story
-                if(entry.slug !== "home") {
-                    const page = {
-                        path: `/${entry.node.full_slug}`,
-                        component: storyblokEntry,
-                        context: {
-                            story: entry.node
-                        }
-                    }
-                    createPage(page)
-                }
-            })
-          })
-        )
-      })
- }
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const allStoryblokEntryQuery = await graphql(`
+    {
+      stories: allStoryblokEntry(filter: { field_component: { eq: "page" } }) {
+        edges {
+          node {
+            id
+            name
+            slug
+            field_component
+            full_slug
+            content
+          }
+        }
+      }
+    }
+  `).then(res => res.data)
+
+  const { edges: stories } = allStoryblokEntryQuery.stories
+
+  const allPages = getByComponent(stories, "page")
+  allPages.forEach(entry => {
+    if (entry.slug === "home") return null
+
+    const page = {
+      path: `/${entry.node.full_slug}`,
+      component: path.resolve("src/templates/page.js"),
+      context: {
+        story: entry.node,
+      },
+    }
+    createPage(page)
+  })
+
+  // const allPosts = getByComponent(stories, "post")
+  // allPosts.forEach(entry => {
+  //   if (entry.slug === "home") return null
+
+  //   const page = {
+  //     path: `/${entry.node.full_slug}`,
+  //     component: path.resolve("src/templates/page.js"),
+  //     context: {
+  //       story: entry.node,
+  //     },
+  //   }
+  //   createPage(page)
+  // })
+}
--- src/lib/storyblok.js
+++ src/lib/storyblok.js
@@ -1,84 +1,85 @@
 import { useEffect, useState } from "react"
-import StoryblokClient from "storyblok-js-client";
-import config from '../../gatsby-config'
-const sbConfig = config.plugins.find((item) => item.resolve === 'gatsby-source-storyblok')
+import StoryblokClient from "storyblok-js-client"
+import sbConfig from "../../storyblok.config"
+
+const TOKEN = process.env.GATSBY_STORYBLOK_TOKEN
 
 const Storyblok = new StoryblokClient({
-  accessToken: sbConfig.options.accessToken,
+  accessToken: TOKEN,
   cache: {
     clear: "auto",
     type: "memory",
   },
-});
+})
 
 export default function useStoryblok(originalStory, location) {
-    let [story, setStory] = useState(originalStory)
+  let [story, setStory] = useState(originalStory)
 
-    if(story && typeof story.content === "string"){
-      story.content = JSON.parse(story.content)
-    }
-    
-    // see https://www.storyblok.com/docs/Guides/storyblok-latest-js
-    function initEventListeners() {
-      const { StoryblokBridge } = window
+  if (story && typeof story.content === "string") {
+    story.content = JSON.parse(story.content)
+  }
 
-      if (typeof StoryblokBridge !== 'undefined') {
-        const storyblokInstance = new StoryblokBridge()
+  // see https://www.storyblok.com/docs/Guides/storyblok-latest-js
+  function initEventListeners() {
+    const { StoryblokBridge } = window
 
-        storyblokInstance.on(['published', 'change'], (event) => {
-          // reloade project on save an publish
-          window.location.reload(true)
-        })  
-    
-        storyblokInstance.on(['input'], (event) => {
-          // live updates when editing
-          if (event.story._uid === story._uid) {
-            setStory(event.story)
-          }
-        }) 
+    if (typeof StoryblokBridge !== "undefined") {
+      const storyblokInstance = new StoryblokBridge(sbConfig)
 
-        storyblokInstance.on(['enterEditmode'], (event) => {
-          // loading the draft version on initial view of the page
-          Storyblok
-            .get(`cdn/stories/${event.storyId}`, {
-              version: 'draft',
-            })
-            .then(({ data }) => {
-              if(data.story) {
-                setStory(data.story)
-              }
-            })
-            .catch((error) => {
-              console.log(error);
-            }) 
-        }) 
-      }
-    }
+      storyblokInstance.on(["published", "change"], () => {
+        // reloade project on save an publish
+        window.location.reload()
+      })
 
-    function addBridge(callback) {
-        // check if the script is already present
-        const existingScript = document.getElementById("storyblokBridge");
-        if (!existingScript) {
-          const script = document.createElement("script");
-          script.src = `//app.storyblok.com/f/storyblok-v2-latest.js`;
-          script.id = "storyblokBridge";
-          document.body.appendChild(script);
-          script.onload = () => {
-            // call a function once the bridge is loaded
-            callback()
-          };
-        } else {
-            callback();
+      storyblokInstance.on(["input"], event => {
+        // live updates when editing
+        if (event.story._uid === story._uid) {
+          setStory(event.story)
         }
+      })
+
+      storyblokInstance.on(["enterEditmode"], event => {
+        // loading the draft version on initial view of the page
+        Storyblok.get(`cdn/stories/${event.storyId}`, {
+          version: "draft",
+          ...sbConfig,
+        })
+          .then(({ data }) => {
+            if (data.story) {
+              setStory(data.story)
+            }
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      })
     }
+  }
 
-    useEffect(() => {
-      // load bridge only inside the storyblok editor
-      if(location.search.includes("_storyblok")) {
-        // first load the bridge and then attach the events
-        addBridge(initEventListeners)
+  function addBridge(callback) {
+    // check if the script is already present
+    const existingScript = document.getElementById("storyblokBridge")
+    if (!existingScript) {
+      const script = document.createElement("script")
+      script.src = `//app.storyblok.com/f/storyblok-v2-latest.js`
+      script.id = "storyblokBridge"
+      document.body.appendChild(script)
+      script.onload = () => {
+        // call a function once the bridge is loaded
+        callback()
       }
-    }, []) // it's important to run the effect only once to avoid multiple event attachment
+    } else {
+      callback()
+    }
+  }
 
-    return story;
+  useEffect(() => {
+    // load bridge only inside the storyblok editor
+    if (location.search.includes("_storyblok")) {
+      // first load the bridge and then attach the events
+      addBridge(initEventListeners)
+    }
+  }, []) // it's important to run the effect only once to avoid multiple event attachment
+
+  return story
 }
