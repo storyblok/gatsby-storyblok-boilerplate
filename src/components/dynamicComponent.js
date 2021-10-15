bb624547cf94da2373b441391d616fb8cc2eb9cd
Arisa Fukuzaki <25793187+schabibi1@users.noreply.github.com>
2021-10-15 20:03:22 +0200

    add @storyblok/storyblok-editable

--- src/components/dynamicComponent.js
+++ src/components/dynamicComponent.js
@@ -1,5 +1,5 @@
-import SbEditable from 'storyblok-react'
-import Teaser from './Teaser'
+import { sbEditable } from "@storyblok/storyblok-editable"
+import Teaser from "./Teaser"
 import React from "react"
 
 const Components = {
@@ -9,7 +9,7 @@ const Components = {
 const DynamicComponent = ({ blok }) => {
   if (typeof Components[blok.component] !== 'undefined') {
     const Component = Components[blok.component]
-    return (<SbEditable content={blok}><Component blok={blok} /></SbEditable>)
+    return (<div {...sbEditable(blok)} content={blok}><Component blok={blok} /></div>)
   }
   return (<p>The component <strong>{blok.component}</strong> has not been created yet.</p>)
 }
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,14 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-import SbEditable from 'storyblok-react'
+import { sbEditable } from "@storyblok/storyblok-editable"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import DynamicComponent from "../components/dynamicComponent"
 import useStoryblok from "../lib/storyblok"
 
-const NotFoundPage = ({ location }) => { 
+const NotFoundPage = ({ location }) => {
   let components = null
   let story = useStoryblok(null, location)
 
@@ -21,22 +21,22 @@ const NotFoundPage = ({ location }) => {
   return (
   <Layout>
     <Seo title="Home" />
-    <SbEditable content={story ? story.content : false }>
-    <h1>{ story ? story.content.title : 'Not Found' }</h1>
-    { components }
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-    </SbEditable>
+    <div {...sbEditable(story)} content={story ? story.content : false }>
+      <h1>{ story ? story.content.title : 'Not Found' }</h1>
+      { components }
+      <StaticImage
+        src="../images/gatsby-astronaut.png"
+        width={300}
+        quality={95}
+        formats={["AUTO", "WEBP", "AVIF"]}
+        alt="A Gatsby astronaut"
+        style={{ marginBottom: `1.45rem` }}
+      />
+      <p>
+        <Link to="/page-2/">Go to page 2</Link> <br />
+        <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+      </p>
+    </div>
   </Layout>
 )}
 
